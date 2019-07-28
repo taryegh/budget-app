db8d70a23a64f48525b537da4cacb57f79d7c1f5
Taron Yeghoyan <taron_yeghoyan17@alumni.aua.am>
2019-07-28 20:33:30 +0400

    Updates

---
+++ precache-manifest.e217ae68b105345a7c3bca9c8009a6b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "535b45f5eb4a9f290b8c469054b63634",
+    "url": "/budget-app-react/index.html"
+  },
+  {
+    "revision": "f90adb1f9207ce24671b",
+    "url": "/budget-app-react/static/css/main.4866fb01.chunk.css"
+  },
+  {
+    "revision": "73d7d2a4fb7760bb5792",
+    "url": "/budget-app-react/static/js/2.8d89fcb1.chunk.js"
+  },
+  {
+    "revision": "f90adb1f9207ce24671b",
+    "url": "/budget-app-react/static/js/main.8ab859ba.chunk.js"
+  },
+  {
+    "revision": "edd80ffd96b40fa3aa3b",
+    "url": "/budget-app-react/static/js/runtime~main.1813f2d0.js"
+  },
+  {
+    "revision": "3e3e2419d111b516573145bbcdf06f42",
+    "url": "/budget-app-react/static/media/budg-img.3e3e2419.jpg"
+  }
+]);
